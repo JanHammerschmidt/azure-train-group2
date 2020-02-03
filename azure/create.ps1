95ae1268aa875e8efe266e7a502602d53eb04100
Ralf Karle <ralf.karle@comma-soft.com>
2020-02-03 13:46:08 +0100

    initial

---
+++ azure/create.ps1
@@ -0,0 +1,20 @@
+﻿$erroractionpreference = "stop"
+
+
+
+$resourceGroup = "gruppe2"
+$hubName = "rg2-iothub"
+
+az account set --subscription "Azure Schulung"
+
+az group create --location "West Europe"   --name $resourceGroup --subscription "Azure Schulung"
+if ( $LASTEXITCODE -ne 0 ) {
+    THROW "Failed to create resourceGroup"
+}
+
+
+az iot hub create --name $hubName  --resource-group $resourceGroup --subscription "Azure Schulung"
+if ( $LASTEXITCODE -ne 0 ) {
+    THROW "Failed to create iothub"
+}
+
